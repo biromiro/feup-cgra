541268bd7320e5bd222a0d5157e8bd2fef893f74
Rui Rodrigues <ruirodrig@fe.up.pt>
2021-02-11 17:34:46 +0000

    Base code for CGRA projects

---
+++ tp1/MyInterface.js
@@ -0,0 +1,28 @@
+import {CGFinterface, dat} from '../lib/CGF.js';
+
+/**
+* MyInterface
+* @constructor
+*/
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        return true;
+    }
+}
---
+++ tp2/README.md
@@ -0,0 +1,10 @@
+# CGRA 2020/2021
+
+## Group T0xG0y
+
+## TP 2 Notes
+
+(add your main observations/remarks about your experiments here, in a bulleted list, and remove this line. Some examples below)
+
+- In exercise 1 we observed X
+- In exercise 2 we had difficulties in Y

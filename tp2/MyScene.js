289b15ab83a930f874b38b7b185e7b15fdf3e6eb
Nuno Costa <n.r.t.c@hotmail.com>
2021-02-27 18:31:04 +0000

    Fixed <base> cube on exercise 3

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -1,7 +1,7 @@
-import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
-import { MyTangram } from "./MyTangram.js";
-import { MyUnitCube } from "./MyUnitCube.js";
-import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
+import { CGFscene, CGFcamera, CGFaxis } from '../lib/CGF.js'
+import { MyTangram } from './MyTangram.js'
+import { MyUnitCube } from './MyUnitCube.js'
+import { MyUnitCubeQuad } from './MyUnitCubeQuad.js'
 
 /**
  * MyScene
@@ -9,42 +9,41 @@ import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
  */
 export class MyScene extends CGFscene {
   constructor() {
-    super();
+    super()
   }
   init(application) {
-    super.init(application);
+    super.init(application)
 
-    this.initCameras();
-    this.initLights();
+    this.initCameras()
+    this.initLights()
 
     //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0)
 
-    this.gl.clearDepth(100.0);
-    this.gl.enable(this.gl.DEPTH_TEST);
-    this.gl.enable(this.gl.CULL_FACE);
-    this.gl.depthFunc(this.gl.LEQUAL);
+    this.gl.clearDepth(100.0)
+    this.gl.enable(this.gl.DEPTH_TEST)
+    this.gl.enable(this.gl.CULL_FACE)
+    this.gl.depthFunc(this.gl.LEQUAL)
 
     //Initialize scene objects
-    this.axis = new CGFaxis(this);
-    this.tangram = new MyTangram(this);
-    this.unitCube = new MyUnitCube(this);
-    this.unitCubeQuad = new MyUnitCubeQuad(this);
+    this.axis = new CGFaxis(this)
+    this.tangram = new MyTangram(this)
+    this.unitCube = new MyUnitCube(this)
+    this.unitCubeQuad = new MyUnitCubeQuad(this)
 
     //Objects connected to MyInterface
-    this.displayAxis = true;
-    this.scaleFactor = 1;
-    this.showTangram = true;
-    this.showUnitCube = true;
-    this.showUnitCubeQuad = true;
-
+    this.displayAxis = true
+    this.scaleFactor = 1
+    this.showTangram = true
+    this.showUnitCube = true
+    this.showUnitCubeQuad = true
   }
 
   initLights() {
-    this.lights[0].setPosition(15, 2, 5, 1);
-    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-    this.lights[0].enable();
-    this.lights[0].update();
+    this.lights[0].setPosition(15, 2, 5, 1)
+    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0)
+    this.lights[0].enable()
+    this.lights[0].update()
   }
 
   initCameras() {
@@ -54,32 +53,31 @@ export class MyScene extends CGFscene {
       500,
       vec3.fromValues(15, 15, 15),
       vec3.fromValues(0, 0, 0)
-    );
+    )
   }
 
   setDefaultAppearance() {
-    this.setAmbient(0.2, 0.4, 0.8, 1.0);
-    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);
+    this.setAmbient(0.2, 0.4, 0.8, 1.0)
+    this.setDiffuse(0.2, 0.4, 0.8, 1.0)
+    this.setSpecular(0.2, 0.4, 0.8, 1.0)
+    this.setShininess(10.0)
   }
-
 
   display() {
     // ---- BEGIN Background, camera and axis setup
     // Clear image and depth buffer everytime we update the scene
-    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height)
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
     // Initialize Model-View matrix as identity (no transformation
-    this.updateProjectionMatrix();
-    this.loadIdentity();
+    this.updateProjectionMatrix()
+    this.loadIdentity()
     // Apply transformations corresponding to the camera position relative to the origin
-    this.applyViewMatrix();
+    this.applyViewMatrix()
 
     // Draw axis
-    if (this.displayAxis) this.axis.display();
+    if (this.displayAxis) this.axis.display()
 
-    this.setDefaultAppearance();
+    this.setDefaultAppearance()
 
     var sca = [
       this.scaleFactor,
@@ -97,35 +95,34 @@ export class MyScene extends CGFscene {
       0.0,
       0.0,
       0.0,
-      1.0,
-    ];
+      1.0
+    ]
 
-
-    this.multMatrix(sca);
+    this.multMatrix(sca)
 
     // ---- BEGIN Primitive drawing section
 
-    this.pushMatrix();
+    this.pushMatrix()
 
-    this.translate(0.5, 0, -0.5);
+    this.translate(2, 0, 2.5)
 
-    this.rotate(-Math.PI / 2, 1, 0, 0);
+    this.rotate(-Math.PI / 2, 1, 0, 0)
 
-    if (this.showTangram) this.tangram.display();
+    if (this.showTangram) this.tangram.display()
 
-    this.pushMatrix();
+    this.pushMatrix()
 
-    this.translate(0, 0, -0.5);
+    this.translate(0, 0, -0.6)
+    this.scale(4, 5, 1)
 
-    if (this.showUnitCube) this.unitCube.display();
+    if (this.showUnitCube) this.unitCube.display()
 
-    if (this.showUnitCubeQuad) this.unitCubeQuad.display();
+    if (this.showUnitCubeQuad) this.unitCubeQuad.display()
 
-    this.popMatrix();
+    this.popMatrix()
 
-    this.popMatrix();
+    this.popMatrix()
 
     // ---- END Primitive drawing section
-
   }
 }
